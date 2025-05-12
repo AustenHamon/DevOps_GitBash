0f4e77fcafca6576349065ce77d2dc27efa1a154
Austen Hamon <austinhamon071@gmail.com>
2025-05-12 12:10:14 +0200

    Added student ID message

--- Nkuna222435267.java
+++ Nkuna222435267.java
@@ -1,5 +1,6 @@
 public class Nkuna222435267 {
     public static void main(String[] args) {
         System.out.println("This is a sicktest");
+        System.out.println("Written on the Monday");
     }
 }
